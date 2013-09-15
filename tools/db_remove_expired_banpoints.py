2aec5b11b29ed472294a08d1c5f99a7b86769059
urthub <info@urthub.de>
2013-09-15 16:25:49 +0200

    Add database tools and scripts

---
+++ tools/db_remove_expired_banpoints.py
@@ -0,0 +1,19 @@
+### IMPORTS
+import sqlite3
+import time
+
+
+### Main ###
+# connect to database
+connnection = sqlite3.connect('../data.sqlite')
+cursor = connnection.cursor()
+
+timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+values = (timestamp,)
+
+# remove expired ban_points
+cursor.execute("DELETE FROM `ban_points` WHERE `expires` < ?", values)
+connnection.commit()
+
+# close database
+connnection.close()
