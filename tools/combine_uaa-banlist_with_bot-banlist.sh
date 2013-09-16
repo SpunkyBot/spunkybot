932d117832796e6cb33129e334e2718d746eca40
urthub <info@urthub.de>
2013-09-16 20:52:17 +0200

    Set path of spunky-bot

--- tools/combine_uaa-banlist_with_bot-banlist.sh
+++ tools/combine_uaa-banlist_with_bot-banlist.sh
@@ -4,6 +4,6 @@
 wget http://www.urtadmins.net/banlist/UAA-banlist-aimbot-only_for-use-without-b3.txt -O /opt/urbanterror/.q3a/q3ut4/uaa-banlist.txt
 
 # merge UAA and Spunky Bot banlist, duplicated entries will be removed
-cat /opt/urbanterror/.q3a/q3ut4/bot-banlist.txt /opt/urbanterror/.q3a/q3ut4/uaa-banlist.txt | uniq > /opt/urbanterror/.q3a/q3ut4/banlist.txt
+cat /opt/urbanterror/.q3a/q3ut4/spunky-bot/bot-banlist.txt /opt/urbanterror/.q3a/q3ut4/uaa-banlist.txt | uniq > /opt/urbanterror/.q3a/q3ut4/banlist.txt
 
 exit 0
